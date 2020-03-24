604b5549524d02f97860a668ec4c7f7aec1b5e7a
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-03-24 14:37:06 +0000

    exercise completed

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,33 @@
 function addVAT(price) {
-    return price + (0.2 * price);
+  return price + 0.2 * price;
+}
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+function makeHalfPrice(price) {
+  return price / 2;
+}
+function countBooks(number) {
+  return number.length;
+}
+
+function isInStock(book) {
+  if (book.quantity > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function getTotalOrderPrice(price, quantity) {
+  return price * quantity + price * quantity * 0.2;
 }
 
 module.exports = {
-    addVAT
+  addVAT,
+  getFullName,
+  makeHalfPrice,
+  countBooks,
+  isInStock,
+  getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,11 +1,48 @@
-const utils = require('../app/bookshop_utils');
+const utils = require("../app/bookshop_utils");
 
 describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
-  
+
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
+  });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.makeHalfPrice", () => {
+  test("return 12 when passed 24", () => {
+    expect(utils.makeHalfPrice(24)).toBe(12);
+  });
+});
+describe("utils.countBooks", () => {
+  test("return total number of books", () => {
+    expect(utils.countBooks(["1", "2", "3"])).toBe(3);
+  });
+});
+describe("utils.isInStock", () => {
+  test("return true when quantity > 0", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 9
+    };
+    expect(utils.isInStock(book)).toBe(true);
+  });
+});
+describe("utils.getTotalOrderPrice", () => {
+  test("return total price with VAT", () => {
+    expect(utils.getTotalOrderPrice(18, 2)).toBe(43.2);
   });
 });
